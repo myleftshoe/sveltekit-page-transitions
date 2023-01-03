ae4510ab26bb085b266f7cbadc480ac06c22a375
myleftshoe <paulesammut@gmail.com>
2023-01-03 12:32:26 +1100

    Simplied PageTransitionController logic

--- page-transition-controller.svelte
+++ page-transition-controller.svelte
@@ -1,34 +1,21 @@
 <script>
-    import { beforeNavigate, afterNavigate } from '$app/navigation'
+    import { beforeNavigate } from '$app/navigation'
     import { back, transition as storeTransition } from './store'
 
     export let transition = $storeTransition
     $storeTransition = transition
 
-    let updateBack = true
-    function setBack(condition = false) {
-        if (!updateBack) return
-        updateBack = false
-        $back = condition
-    }
+    let ref
+    let popped = false
 
     beforeNavigate(({ to }) => {
-        setBack(to.url.pathname === '/' || to.url.pathname === '/home')
+        $back = popped || to.url.pathname === '/' || to.url.pathname === '/home'
         ref.firstElementChild.style.zIndex = $back ? 1 : 0
+        popped = false
     })
-
-    afterNavigate(() => {
-        updateBack = true
-    })
-
-    function popState(e) {
-        setBack(true)
-    }
-
-    let ref
 </script>
 
-<svelte:window on:popstate={popState} />
+<svelte:window on:popstate={() => (popped = true)} />
 
 <div bind:this={ref}>
     <slot />
